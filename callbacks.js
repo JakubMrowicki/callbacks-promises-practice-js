700c897e978c3ad075e836f44f207c6dfe3452f1
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2022-01-21 19:19:38 +0000

    callbacks

---
+++ callbacks.js
@@ -0,0 +1,30 @@
+const posts = [
+    {title: "Post 1", body: "This is post 1"},
+    {title: "Post 2", body: "This is post 2"}
+]
+
+const getPosts = () => {
+    setTimeout(() => {
+        let output = ''
+        posts.forEach(post => {
+            output += `<li>${post.title}</li>`
+        })
+        document.body.innerHTML = output
+    }, 1000)
+}
+
+// const createPost = (post) => {
+//     setTimeout(() => {
+//         posts.push(post)
+//     }, 2000);
+// }
+
+const createPost = (post, callback) => {
+    setTimeout(() => {
+        posts.push(post)
+        callback()
+    }, 2000);
+}
+
+// getPosts()
+createPost({title: "Post 3", body: "This is post 3"}, getPosts)
